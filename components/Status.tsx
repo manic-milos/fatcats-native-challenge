ce91b4f62edadebc4d6727faf958421dc70dd9ca
milos <milos.manic@rgf.bg.ac.rs>
2022-08-07 23:44:26 +0200

    small fix, view styling on Status comp

--- components/Status.tsx
+++ components/Status.tsx
@@ -3,31 +3,22 @@ import { Text, View } from 'react-native';
 
 function Status({ status } : {status: string}) {
 	return (
-		<View
-			style={{
-				marginTop: 5,
-				marginBottom: 5,
-				marginLeft: 15,
-				marginRight: 20,
-				padding: 10,
-				paddingLeft: 15,
-				paddingRight: 15,
-				borderRadius: 5,
-			}}
-		>
-			<View style={{ flex: 1 }}>
-				<Text
-					style={{
-						fontWeight: '500',
-						borderWidth: 1,
-						borderColor: '#eee',
-						padding: 5,
-						backgroundColor: status === 'active' ? '#34be3b' : '#fff0',
-					}}
-				>
-					{status}
-				</Text>
-			</View>
+		<View>
+			<Text
+				style={{
+					marginTop: 15,
+					marginBottom: 15,
+					marginLeft: 25,
+					marginRight: 30,
+					fontWeight: '500',
+					borderWidth: 1,
+					borderColor: '#eee',
+					padding: 5,
+					backgroundColor: status === 'active' ? '#34be3b' : '#fff0',
+				}}
+			>
+				{status}
+			</Text>
 		</View>
 	);
 }
